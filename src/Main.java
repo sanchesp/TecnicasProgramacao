addad3470ce11b7325735c434a281492bba271b2
Marcos <marcossanches1@hotmail.com.br>
2022-09-07 14:54:14 -0300

    inclusão do exercicios 3, 4, 8 e 9

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,85 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.time.Year;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoField;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Path path = Paths.get("campeonato-brasileiro-full.csv");
 
-        quantidadePartidas(path);
+        Stream<String> stream = null;
+
+        //Exercicio8
+        try {
+            stream = Files.lines(path);
+            String vitoriaMandante = String.valueOf(stream
+                    .skip(1)
+                    .map(table -> mapToTabela(table))
+                    .filter((mandante) -> mandante.getMandante().equals(mandante.getVencedor()))
+                    .count());
+            System.out.println(vitoriaMandante);
+        } catch (IOException e) {
+
+            throw new RuntimeException(e);
+        }
+
+        //Exercicio9
+        try {
+            stream = Files.lines(path);
+            String vitoriaVisitante = String.valueOf(stream
+                    .skip(1)
+                    .map(table -> mapToTabela(table))
+                    .filter((visitante) -> visitante.getVisitante().equals(visitante.getVencedor()))
+                    .count());
+                        System.out.println(vitoriaVisitante);
+        } catch (IOException e) {
+
+            throw new RuntimeException(e);
+        }
+
+
 
     }
+
+
 
 
 
     //Exercicio1
     private static void quantidadePartidas(Path path) throws IOException {
-        Stream<String> stream;
-        stream = Files.lines(path);
-                int soma = (int) stream
+        Stream<String> stream = Files.lines(path);
+        int soma = (int) stream
                 .skip(1)
                 .map(table -> mapToTabela(table))
                 .count();
         System.out.println("De 2003 à 2021 foram realizadas " + soma + " partidas");
     }
+    //Exercicio3
+    private static void quantidadePartidas10E15(Path path) throws IOException {
+        Stream<String> stream;
+        stream = Files.lines(path);
+        int soma = (int) stream
+                .skip(1)
+                .map(table -> mapToTabela(table))
+                .filter(table -> table.getData().getYear() >= 2010)
+                .filter(table -> table.getData().getYear() <= 2015)
+                .count();
+        System.out.println("De 2010 à 2015 foram realizadas " + soma + " partidas");
+    }
+    //Exercicio4
+    private static void maxGols(Path path) throws IOException {
+        Stream<String> stream = Files.lines(path);
+        int maxGols = (int) stream
+                .skip(1)
+                .map(table -> mapToTabela(table))
+                .mapToInt(gols -> gols.getMandantePlacar() + gols.getVisitantePlacar())
+                .max().getAsInt();
+        System.out.println(maxGols);
+    }
+
 
     private static ResultadosCampeonato mapToTabela (String resultadosCampeonato){
 
